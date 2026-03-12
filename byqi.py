536ba0d71dd2a07cbdf5d4348ec25edbcf785435
Rehaaan Das <dudeamazing5609@gmail.com>
2026-03-12 18:44:15 +0530

    Added constants, fixed bracket expansion, removed negative if statements, and added an exit function

--- byqi.py
+++ byqi.py
@@ -49,7 +49,22 @@ for index, line in enumerate(program):
 for index, line in enumerate(program):
     if(line.replace(" ", "")[0:2] == "//"):
         del program[index]
-                    
+
+CONSTANTS = [   ["DR_A_HI" , "*(65529)"], 
+                [ "DR_A_LO" , "*(65530)"],
+                [ "DR_DATA" , "*(65531)"],
+                [ "DR_DONE" , "*(65532)"],
+                [ "DW_E" , "*(65524)"],
+                [ "DW_A_HI" , "*(65525)"],
+                [ "DW_A_LO" , "*(65526)"],
+                [ "DW_DATA" , "*(65527)"],
+                [ "DW_DONE" , "*(65528)"]
+            ]
+
+for index, line in enumerate(program):
+    for const in CONSTANTS:
+        program[index] = program[index].replace(const[0], const[1])
+
 #goto label assignment
 for index, line in enumerate(program):
     if(re.fullmatch(r'.+\:', line.replace(" ", ""))):
@@ -123,15 +138,18 @@ for index, line in enumerate(program):
         codepointCounter += 1
 #bracket expansion
 for index, line in enumerate(program):
-    start = re.search(r'[\s*&\(]\(', line)
-    if(start):
-        end = findClosingBracket(line, start.end())+1
-        simplified = line.replace(line[start.end()-1:end+1], "t0000")
+    start = re.search(r'[\s\*\&\(\!]\(', program[index])
+    while(start):
+        #for l in program:
+        #    print(l)
+        end = findClosingBracket(program[index], start.end())+1
+        simplified = program[index].replace(program[index][start.end()-1:end+1], "t0000")
         program.insert(index+1, simplified)
 
-        program[index] = f"t0000 = {line[start.end():end]}"
+        program[index] = f"t0000 = {program[index][start.end():end]}"
 
         if(1 > tvarsCounter): tvarsCounter = 1
+        start = re.search(r'[\s\*\&\(\!]\(', program[index])
 #tvar init lines
 for i in range(0, tvarsCounter+1):
     program.insert(0, f"t{i:04}")
@@ -219,14 +237,11 @@ if not simplifyOnly:
                 checkInstructions = [f"LDI R6 {splitLine[1]}",
                                     "ADD R6 R6 R6",
                                     f"JZI {splitLine[2]}"]
-            else:
-                checkInstructions = [f"LDI R6 {splitLine[1][1:]}",
-                                    "ADD R6 R6 R6",
-                                    f"JZI e{codepointCounter:04}",
-                                    f"JUCI {splitLine[2]}",
-                                    f"e{codepointCounter:04}"]
+                
+            #to the absolutely nobody who is going to read this,
+            #there used to be !negation functionality...
+
             program = insertSubArray(program, index, checkInstructions)
-            codepointCounter += 1
         #e0001(e0003, e0004)
         if(re.fullmatch(r'e[0-9][0-9][0-9][0-9]\(.*\)', line.replace(" ", ""))):
             splitLine = re.split(r'[\(\)]', line.replace(" ", ""))
@@ -326,6 +341,9 @@ if not simplifyOnly:
         #goto e0000
         if(re.fullmatch(r'goto.*', line.replace(" ", ""))):
             program = insertSubArray(program, index, ["JUCI " + line.replace(" ", "").split("goto")[1]])
+        #exit()
+        if(re.fullmatch(r'exit\(\)', line.replace(" ", ""))):
+            program = insertSubArray(program, index, ["HLT"])
 
     program = ["REG R14 1", f"REG R4 e{codepointCounter:04}"] + program + [f"e{codepointCounter:04}"]
 
